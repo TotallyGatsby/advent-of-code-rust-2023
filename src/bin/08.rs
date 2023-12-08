2c7c017c917de473eb05adbad5dd662824fa2aec
TotallyGatsby <700673+TotallyGatsby@users.noreply.github.com>
2023-12-08 13:14:29 -0800

    Day 8 done

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -7,21 +7,21 @@ advent_of_code::solution!(8);
 struct Cursor<'a> {
     pub instructions: String,
     pub current_node: Option<&'a PathNode>,
+    pub start: String,
 }
 
 impl<'a> Cursor<'a> {
-    pub fn walk(mut self, start: &str, node_map: &'a HashMap<String, PathNode>) -> Option<u32> {
-        self.current_node = node_map.get(&start.to_string());
+    pub fn walk(&mut self, node_map: &'a HashMap<String, PathNode>) -> Option<u32> {
+        self.current_node = node_map.get(&self.start);
         let mut steps = 0;
-        while self.current_node.is_some() && self.current_node?.name != "ZZZ" {
+
+        while self.current_node.is_some() && !self.current_node?.name.ends_with("Z") {
             let instruction = self.instructions.remove(0);
             match instruction {
                 'R' => {
-                    println!("Right");
                     self.current_node = node_map.get(&self.current_node?.conns[1]);
                 }
                 'L' => {
-                    println!("Left");
                     self.current_node = node_map.get(&self.current_node?.conns[0]);
                 }
                 _ => {
@@ -59,9 +59,10 @@ impl PathNode {
     }
 }
 pub fn part_one(input: &str) -> Option<u32> {
-    let cursor = Cursor {
+    let mut cursor = Cursor {
         instructions: input.lines().nth(0).unwrap().to_string(),
         current_node: None,
+        start: "AAA".to_string(),
     };
 
     let mut node_map: HashMap<String, PathNode> = HashMap::new();
@@ -73,11 +74,52 @@ pub fn part_one(input: &str) -> Option<u32> {
         }
     });
 
-    cursor.walk("AAA", &node_map)
+    cursor.walk(&node_map)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+fn gcd(a: u64, b: u64) -> u64 {
+    if b == 0 {
+        a
+    } else {
+        gcd(b, a % b)
+    }
+}
+
+fn lcm(a: u64, b: u64) -> u64 {
+    (a * b) / gcd(a, b)
+}
+
+fn lcm_of_list(numbers: Vec<u32>) -> u64 {
+    numbers.iter().fold(1, |acc, n| lcm(acc, *n as u64))
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let instructions = input.lines().nth(0).unwrap().to_string();
+
+    let mut node_map: HashMap<String, PathNode> = HashMap::new();
+    let mut cursors: Vec<Cursor> = vec![];
+    input.lines().enumerate().for_each(|(idx, line)| {
+        if idx >= 2 {
+            let node = PathNode::new(line);
+            if node.name.ends_with("A") {
+                cursors.push(Cursor {
+                    instructions: instructions.clone(),
+                    current_node: None,
+                    start: node.name.clone(),
+                });
+            }
+            node_map.insert(node.name.clone(), node.clone());
+        }
+    });
+
+    println!("Found {} cursors.", cursors.len());
+
+    let step_counts = cursors
+        .iter_mut()
+        .map(|cursor| cursor.walk(&node_map).unwrap())
+        .collect_vec();
+
+    Some(lcm_of_list(step_counts))
 }
 
 #[cfg(test)]
@@ -93,6 +135,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2));
     }
 }
