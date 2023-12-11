e277a6271d98cceb2942dd0cedb7158c6df3e147
TotallyGatsby <700673+TotallyGatsby@users.noreply.github.com>
2023-12-11 11:56:36 -0800

    Day 11 part 2 done

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -29,6 +29,50 @@ pub fn part_one(input: &str) -> Option<usize> {
         }
     });
 
+    Some(
+        galaxy_points
+            .iter()
+            .combinations_with_replacement(2)
+            .fold(0, |acc, c| {
+                let point_a = c[0];
+                let point_b = c[1];
+
+                if point_a == point_b {
+                    return acc;
+                }
+
+                acc + point_a.0.abs_diff(point_b.0) + point_a.1.abs_diff(point_b.1)
+            }),
+    )
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let mut galaxy_points = vec![];
+
+    let mut y_nudge = 0;
+    let mut x_nudge = 0;
+    input.lines().enumerate().for_each(|(y, row)| {
+        let mut found_galaxy = false;
+        x_nudge = 0;
+        row.chars().enumerate().for_each(|(x, char)| {
+            // This is so inefficient, lol
+            if !input
+                .lines()
+                .any(|line| line.chars().nth(x).unwrap() == '#')
+            {
+                x_nudge += 999999;
+            }
+
+            if char == '#' {
+                galaxy_points.push((x + x_nudge, y + y_nudge));
+                found_galaxy = true;
+            }
+        });
+        if !found_galaxy {
+            y_nudge += 999999;
+        }
+    });
+
     println!("{:?}", galaxy_points);
 
     Some(
@@ -48,10 +92,6 @@ pub fn part_one(input: &str) -> Option<usize> {
     )
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -65,6 +105,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(82000210));
     }
 }
